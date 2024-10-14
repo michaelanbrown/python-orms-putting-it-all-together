e0878ce32fdbdd8348ab5fcf99973b30970034f5
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-14 15:15:52 -0400

    id defaults to None in the init method

--- lib/dog.py
+++ lib/dog.py
@@ -4,7 +4,7 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    def __init__(self, name, breed, id):
+    def __init__(self, name, breed, id=None):
         self.name = name
         self.breed = breed
         self.id = id
